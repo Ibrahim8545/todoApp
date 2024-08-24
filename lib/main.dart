d267799a2406abf1f05d0529a740e134c610b8b4
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-08-24 18:47:58 +0300

    add shred preference and multi providers

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,32 @@ import 'package:provider/provider.dart';
 import 'package:todoapp/firebase_options.dart';
 import 'package:todoapp/home_screen.dart';
 import 'package:todoapp/providers/my_provider_auth.dart';
+import 'package:todoapp/providers/theme_provider.dart';
 import 'package:todoapp/signin_and_sigup/login.dart';
 import 'package:todoapp/signin_and_sigup/regsiter.dart';
 import 'package:todoapp/splash_screen.dart';
 import 'package:todoapp/widget/edit_task.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
 //await FirebaseFirestore.instance.disableNetwork();
-  runApp(ChangeNotifierProvider(
-    create: (context) =>MyProvider(),
-    child: const MyApp()));
+  runApp(
+    MultiProvider(providers:[
+       ChangeNotifierProvider(
+      create: (context) => MyProvider(),
+       ),
+       ChangeNotifierProvider(
+        create:(context)=> ThemeProvider()
+        )
+    ],
+        child: const MyApp(),
+    )
+  
+  
+    );
 }
 
 class MyApp extends StatelessWidget {
@@ -29,13 +41,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const  SplashScreen(),
-      routes:{
-        HomeScreen.routeName:(context) =>  HomeScreen(),
-        LoginPage.routeName:(context) => LoginPage(),
-        SignupScreen.routeName:(context) => SignupScreen(),
-        EditTask.routeName :(context) => EditTask(),
-
+      home: const SplashScreen(),
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        LoginPage.routeName: (context) => LoginPage(),
+        SignupScreen.routeName: (context) => SignupScreen(),
+        EditTask.routeName: (context) => EditTask(),
       },
     );
   }
---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier
+{
+  ThemeMode appTheme = ThemeMode.light;
+
+  MyProvider() {
+    loadThemeFromPrefs();
+  }
+
+  void changeTheme(ThemeMode themeMode) async {
+    appTheme = themeMode;
+    notifyListeners();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('themeMode', themeMode == ThemeMode.light ? 'light' : 'dark');
+  }
+
+  void loadThemeFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? theme = prefs.getString('themeMode');
+    if (theme != null) {
+      appTheme = theme == 'light' ? ThemeMode.light : ThemeMode.dark;
+      notifyListeners();
+    }
+  }
+}
